5927dbaa096f908aff4bb99eecdffe2ec07e8529
Shinichi Kanehira <skanehir1126@gmail.com>
2024-05-06 17:32:42 +0900

    update

--- src/extract_exif/cli.py
+++ src/extract_exif/cli.py
@@ -17,31 +17,55 @@ def get_parser():
     return parser
 
 
+def extract(file):
+    exif_info_all = read_image(file)
+
+    exif_info = extract_info(exif_info_all)
+    # 露出時間を分数表記に変換
+    try:
+        exif_info["露出時間（分数）"] = to_fraction(float(exif_info["露出時間"]))
+    except:
+        exif_info["露出時間（分数）"] = "Not Found"
+
+    exif_info["ファイルパス"] = file
+
+    return exif_info
+
+
 def cli():
     parser = get_parser()
 
     args = parser.parse_args()
 
-    output_dict = {}
+    output_list = []
     for file in tqdm(args.file):
-
-        exif_info_all = read_image(file)
-
-        exif_info = extract_info(exif_info_all)
-        # 露出時間を分数表記に変換
-        try:
-            exif_info["露出時間（分数）"] = to_fraction(float(exif_info["露出時間"]))
-        except:
-            exif_info["露出時間（分数）"] = "Not Found"
-
-        output_dict[os.path.basename(file)] = exif_info
+        if os.path.isdir(file):
+            for name in os.listdir(file):
+                if name.endswith(".jpeg") or name.endswith(".jpg"):
+                    file_path = os.path.join(file, name)
+                    exif_info = extract(file_path)
+                    exif_info["ファイル名"] = name
+                    output_list.append(exif_info)
+        elif file.endswith(".jpeg") or file.endswith(".jpg"):
+            exif_info = extract(file)
+            exif_info["ファイル名"] = os.path.basename(file)
+            output_list.append(exif_info)
+        else:
+            continue
 
     if platform.system() == "Windows":
         encode = "shift-jis"
     else:
         encode = "utf-8"
 
-    pd.DataFrame(output_dict).T.sort_index().to_csv(
+    df_exif_info = pd.DataFrame(output_list).set_index("ファイル名")
+    # 並び替え
+    backward_columns = ["ファイルパス"]
+    df_exif_info = df_exif_info[
+        [col for col in df_exif_info.columns if col not in backward_columns]
+        + [col for col in df_exif_info.columns if col in backward_columns]
+    ]
+    df_exif_info.sort_index().to_csv(
         "写真情報.csv",
         encoding=encode,
     )
