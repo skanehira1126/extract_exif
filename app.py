bc3e45dc302194809d5556dbd344f756753b74da
Shinichi Kanehira <shinichi.kanehira@gmail.com>
2024-05-12 23:07:14 +0900

    update

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import base64
 import io
 import platform
+import zipfile
 
 import pandas as pd
 import streamlit as st
@@ -36,7 +37,8 @@ def file_preview():
     if len(uploaded_files) >= 1:
         image_files = []
         for _file in uploaded_files:
-            image = Image.open(io.BytesIO(_file.getvalue()))
+            raw_image = _file.getvalue()
+            image = Image.open(io.BytesIO(raw_image))
 
             # タグの取得
             exif_info_all = image.getexif()
@@ -47,6 +49,7 @@ def file_preview():
             image_files.append(
                 {
                     "image": image,
+                    "raw_image": raw_image,
                     "file_id": _file.file_id,
                     "name": _file.name,
                 }
@@ -84,7 +87,7 @@ def make_image_df(image_files):
     image_df = pd.DataFrame(
         [
             {
-                "image preview": f"data:image/jpg;base64,{base64.b64encode(get_compressed_image_bytes(img['image'])).decode()}",
+                "プレビュー": f"data:image/jpg;base64,{base64.b64encode(get_compressed_image_bytes(img['image'])).decode()}",
                 "file_id": img["file_id"],
                 "ファイル名": img["name"],
                 "露出時間": img["露出時間"],
@@ -95,8 +98,23 @@ def make_image_df(image_files):
             }
             for img in image_files
         ]
-    ).drop_duplicates(["image preview"], keep="first")
+    ).drop_duplicates(["プレビュー"], keep="first")
+
     return image_df
+
+
+def get_binary_of_images(uploaded_files):
+    bytes_io = io.BytesIO()
+
+    with zipfile.ZipFile(bytes_io, "w") as zipf:
+        for file in filter(
+            lambda x: x["file_id"] in st.session_state.selected_images, uploaded_files
+        ):
+            zipf.writestr(file["name"], file["raw_image"])
+
+    bytes_io.seek(0)
+
+    return bytes_io.getvalue()
 
 
 initialize()
@@ -109,7 +127,10 @@ update_images = set(st.session_state.current_images) != set(
 
 st.session_state.current_images = [img["file_id"] for img in image_files]
 
+
 if len(image_files) >= 1:
+    if st.button("全選択"):
+        st.session_state.image_df["選択"] = True
     if update_images:
         st.session_state.image_df = make_image_df(image_files)
 
@@ -117,18 +138,36 @@ if len(image_files) >= 1:
         st.session_state.image_df,
         column_config={
             "file_id": None,
-            "image preview": st.column_config.ImageColumn(),
+            "プレビュー": st.column_config.ImageColumn(),
             "選択": st.column_config.CheckboxColumn(),
         },
         disabled=[col for col in st.session_state.image_df.columns if col != "選択"],
+        column_order=[
+            "選択",
+            "プレビュー",
+            "ファイル名",
+            "露出時間",
+            "ISO感度",
+            "焦点距離",
+            "F値",
+        ],
         hide_index=True,
-        height=300,
+        height=250,
         use_container_width=True,
     )
 
     st.session_state.selected_images = [
         file_id for file_id in output_df[output_df["選択"]]["file_id"].values
     ]
+
+st.download_button(
+    "選択した画像のみダウンロード",
+    data=get_binary_of_images(uploaded_files=image_files),
+    file_name="images.zip",
+    mime="application/zip",
+    disabled=len(st.session_state.selected_images) == 0,
+    help="選択列で画像を選択してください",
+)
 
 
 if st.session_state.selected_images:
